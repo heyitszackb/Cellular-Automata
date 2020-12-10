11aff1345697f9241e5f61d025948fe08d3ea2cd
Zack Brandon <goldstellar34@gmail.com>
2020-12-10 10:03:47 -0500

    Added cellboard.js and organized code

---
+++ cellboard.js
@@ -0,0 +1,181 @@
+class CellBoard {
+    constructor() {
+      this.cells = []
+    }
+    //display all the cells in this.cells
+    showCells() {
+      //Check every row
+      for (let i = 0; i < ROWS;i++)  {
+          //Check every column
+          for (let j = 0; j < COLS;j++)  {
+            //Check the neighboring cells around the one you are looking at to draw it correctly
+            let hoodCells = []
+            for (let x = -1; x < 2; x++) {
+              for (let y = -1;y < 2; y++) {
+                   //WRAP AROUND BOARD CODE to check the opposite row or column if you are currently on the edge of the board
+                  /*
+                  BOARD CHECKING ORDER ILLUSTRATION
+                  [2] [5] [8]
+                  [1] [4] [7]
+                  [0] [3] [6]
+                  */
+                  let checkRow = (i + x + ROWS) % ROWS
+                  let checkCol = (j + y + COLS) % COLS
+                  //store the states of the surrounding cells in the hoodCells array.
+                  hoodCells.push(this.cells[checkRow][checkCol].state)
+              }
+           }
+            //The variable "hoodCells" now contains a list of the neighboring cell's states; we can therefore use these to calculate
+            //how the cell should be displayed based on these 9 values (one for each of the surrounding cells and the current cell)
+            //NOTE that hoodCell[4] is YOUR current state
+            //****Remeber the the computer in this program counts cells from bottom to top, left to right!****
+            
+            //Create a variable holding all the states that a cell could be
+            let states = ["virus","void"]
+            
+            //Loop through all the states and display the cells based on the states/
+            //This code ensures that only the same states code will "Connect" with itself visually.
+            for (let k = 0;k < states.length;k++) {
+              if (hoodCells[1] == states[k] &&
+                       hoodCells[4] == states[k] &&
+                       hoodCells[7] == states[k]) {
+                       this.cells[i][j].show("left-right")
+                       break
+            } else if (hoodCells[3] == states[k] &&
+                       hoodCells[4] == states[k] &&
+                       hoodCells[5] == states[k]) {
+                       this.cells[i][j].show("up-down")
+                       break
+            } else if (hoodCells[1] == states[k] &&
+                       hoodCells[3] != states[k] &&
+                       hoodCells[4] == states[k] &&
+                       hoodCells[5] != states[k] &&
+                       hoodCells[7] != states[k]) {
+                       this.cells[i][j].show("left")
+                       break
+             } else if (hoodCells[1] != states[k] &&
+                       hoodCells[3] != states[k] &&
+                       hoodCells[4] == states[k] && 
+                       hoodCells[5] != states[k] &&
+                       hoodCells[7] == states[k]) {
+                       this.cells[i][j].show("right")
+                       break
+             } else if (hoodCells[1] != states[k] &&
+                       hoodCells[3] != states[k] &&
+                       hoodCells[4] == states[k] &&
+                       hoodCells[5] == states[k] &&
+                       hoodCells[7] != states[k]) {
+                       this.cells[i][j].show("up")
+                       break
+                } else if (hoodCells[1] != states[k] &&
+                       hoodCells[3] == states[k] &&
+                       hoodCells[4] == states[k] &&
+                       hoodCells[5] != states[k] &&
+                       hoodCells[7] != states[k]) {
+                       this.cells[i][j].show("down")
+                       break
+            } else if (hoodCells[1] != states[k] &&
+                       hoodCells[3] != states[k] &&
+                       hoodCells[4] == states[k] &&
+                       hoodCells[5] == states[k] &&
+                       hoodCells[7] == states[k]) {
+                       this.cells[i][j].show("down-right")
+                       break
+            } else if (hoodCells[1] != states[k] &&
+                       hoodCells[3] == states[k] &&
+                       hoodCells[4] == states[k] &&
+                       hoodCells[5] != states[k] &&
+                       hoodCells[7] == states[k]) {
+                       this.cells[i][j].show("up-right")
+                       break
+            } else if (hoodCells[1] == states[k] &&
+                       hoodCells[3] != states[k] &&
+                       hoodCells[4] == states[k] &&
+                       hoodCells[5] == states[k] &&
+                       hoodCells[7] != states[k]) {
+                       this.cells[i][j].show("down-left")
+                       break
+            } else if (hoodCells[1] == states[k] &&
+                       hoodCells[3] == states[k] &&
+                       hoodCells[4] == states[k] &&
+                       hoodCells[5] != states[k] &&
+                       hoodCells[7] != states[k]) {
+                       this.cells[i][j].show("up-left")
+              }
+            }
+        }
+      }
+    }
+    //create a randomly generated cell block with random states.
+    createRandomCells() {
+        for (let i = 0; i < ROWS; i++) {
+        let row = []
+            for (let j = 0; j < COLS; j++) {
+               let state = random(["void","virus"])
+               let c = new Cell(CELL_SIZE*i,CELL_SIZE*j,CELL_SIZE,state)
+               row.push(c)
+        }
+        this.cells.push(row)
+      }
+    }
+    //create a blank cell block ...All cells are VOID state...
+    createBlankCells() {
+        for (let i = 0; i < ROWS; i++) {
+            let row = []
+            for (let j = 0; j < COLS; j++) {
+                let c = new Cell(CELL_SIZE*i,CELL_SIZE*j,CELL_SIZE,"void")
+                row.push(c)
+        }
+        this.cells.push(row)
+    }
+      
+    }
+  }
+
+//The building block of the entire program. A singular Cell Class
+
+  class Cell {
+    constructor(x,y,size,state) {
+      this.x = x
+      this.y = y
+      this.size = size
+      this.color = 0
+      this.state = state
+    }
+    show(pos) {
+      //Eliminate the lines from the outside of the shapes
+      noStroke()
+      //If the state is 
+      if (this.state == "virus") { 
+          //Void cells are black
+          this.color = VIRUS_COLOR
+        } else {
+          //Virus cells are stored in the variable VIRUS_COLOR
+          this.color = [0,0,0]
+        } 
+      //Fill with specified color based on state (Virus or Void)
+      fill(this.color)
+      //Check the position in relation to the other cells around the current one and draw appropriately
+      if (pos == "left-right" || pos == "up-down") {
+        rect(this.x,this.y,this.size,this.size)
+      } else if (pos == "default") {
+        rect(this.x,this.y,this.size,this.size,RES)
+      } else if (pos == "left") {
+        rect(this.x,this.y,this.size,this.size,0,RES,RES,0)
+      } else if (pos == "right") {
+        rect(this.x,this.y,this.size,this.size,RES,0,0,RES)
+      } else if (pos == "up") {
+        rect(this.x,this.y,this.size,this.size,RES,RES,0,0)
+      } else if (pos == "down") {
+        rect(this.x,this.y,this.size,this.size,0,0,RES,RES)
+      } else if (pos == "down-right") {
+        rect(this.x,this.y,this.size,this.size,RES,0,0,0)
+      } else if (pos == "up-right") {
+        rect(this.x,this.y,this.size,this.size,0,0,0,RES)
+      } else if (pos == "down-left") {
+        rect(this.x,this.y,this.size,this.size,0,RES,0,0)
+      } else if (pos == "up-left") {
+        rect(this.x,this.y,this.size,this.size,0,0,RES,0)
+      }
+      }
+  }
